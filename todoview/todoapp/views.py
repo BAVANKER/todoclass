dc96f23d01bae108b516b2db677954207ab0947e
BAVANKER <bavankerjn@gmail.com>
2023-07-04 16:57:16 +0530

    added

---
+++ todoview/todoapp/views.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from .forms import TodoForm
+from .models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'home.html'
+    context_object_name = 'task1'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'detail.html'
+    context_object_name = 'task'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'cbvupdate.html'
+    context_object_name = 'task'
+    fields = ('name', 'priority', 'date')
+
+    def get_success_url(self):
+        return reverse_lazy('todoapp:cbvdetail', kwargs={'pk': self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('todoapp:cbvhome')
+
+
+def add(request):
+    task1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('task', '')
+        priority = request.POST.get('priority', '')
+        date = request.POST.get('date', '')
+        task = Task(name=name, priority=priority, date=date)
+        task.save()
+    return render(request, 'home.html', {'task1': task1})
+
+
+def detail(request):
+    task = Task.objects.all()
+    return render(request, 'detail.html', {'task': task})
+
+
+def update(request, id):
+    task = Task.objects.get(id=id)
+    f = TodoForm(request.POST or None, instance=task)
+    if f.is_valid():
+        f.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'f': f, 'task': task})
+
+
+def delete(request, id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=id)
+        task.delete()
+        return redirect('/')
+    return render(request, 'delete.html')
